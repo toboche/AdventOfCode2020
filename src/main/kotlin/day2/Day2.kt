163d4738b4f3035e162b3dd677091f889361eb63
toboche <toboche@gmail.com>
2021-09-07 21:32:04 +0200

    Day 2 task 2

--- src/main/kotlin/day2/Day2.kt
+++ src/main/kotlin/day2/Day2.kt
@@ -1,12 +1,19 @@
 package day2
 
 class Day2 {
-    fun task1(input: String): Int {
-        return input.split("\n")
-            .map {
-                val (min, max, c, pass) = it.split("-", " ", ": ")
-                (min.toInt()..max.toInt()).contains(pass.count { it == c.first() })
-                    .let { if (it) 1 else 0 }
-            }.sum()
-    }
+    fun task1(input: String) = input.split("\n")
+        .map {
+            val (min, max, c, pass) = it.split("-", " ", ": ")
+            (min.toInt()..max.toInt()).contains(pass.count { it == c.first() })
+                .let { if (it) 1 else 0 }
+        }.sum()
+
+    fun task2(input: String) = input.split("\n")
+        .map {
+            val (firstS, secondS, cS, pass) = it.split("-", " ", ": ")
+            val first = pass[firstS.toInt() - 1]
+            val second = pass[secondS.toInt() - 1]
+            val c = cS.first()
+            if ((first == c).xor(second == c)) 1 else 0
+        }.sum()
 }
--- src/test/kotlin/day2/Day2Test.kt
+++ src/test/kotlin/day2/Day2Test.kt
@@ -6,7 +6,7 @@ import kotlin.test.assertEquals
 
 class Day2Test {
     @org.junit.jupiter.api.Test
-    fun sampleTest() {
+    fun sample1Test() {
         val input = "1-3 a: abcde\n" +
                 "1-3 b: cdefg\n" +
                 "1-3 b: b\n" +
@@ -21,13 +21,35 @@ class Day2Test {
     }
 
     @org.junit.jupiter.api.Test
-    fun taskTest() {
+    fun task1Test() {
         val input = File("src/test/resources/exampleDay2.txt")
             .readText()
 
         assertEquals(
             607,
             Day2().task1(input)
+        )
+    }
+
+    @org.junit.jupiter.api.Test
+    fun sample2Test() {
+        val input = "1-3 a: abcde\n" +
+                "1-3 b: cdefg\n" +
+                "2-9 c: ccccccccc"
+        assertEquals(
+            1,
+            Day2().task2(input)
+        )
+    }
+
+    @org.junit.jupiter.api.Test
+    fun task2Test() {
+        val input = File("src/test/resources/exampleDay2.txt")
+            .readText()
+
+        assertEquals(
+            321,
+            Day2().task2(input)
         )
     }
 }
