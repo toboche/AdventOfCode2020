5111cd8ff0719908fa80e3a16a7ea86957da5403
Tomasz Bochenek <tomasz.bochenek@dazn.com>
2021-10-24 12:59:23 +0200

    Day 3 task 1 refactor

--- src/main/kotlin/day3/Day3.kt
+++ src/main/kotlin/day3/Day3.kt
@@ -1,11 +1,12 @@
 package day3
 
 class Day3 {
-    fun task1(input: String): Long {
+    fun task1(input: String, rowStep: Int = 1, colStep: Int = 3): Long {
         val rows = input.lines()
         val columns = rows.first().length
         return rows.indices
-            .map { it to (it * 3) % columns }.sumOf { (row, col) ->
+            .filter { it % rowStep == 0 }
+            .map { it to (it * colStep) % columns }.sumOf { (row, col) ->
                 if (rows[row][col] == '#')
                     1L
                 else
