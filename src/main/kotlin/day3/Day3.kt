49b764841a18aef422a88642df0c9ffcb80194e8
toboche <toboche@gmail.com>
2021-10-24 13:14:00 +0200

    Day 3 task 2

--- src/main/kotlin/day3/Day3.kt
+++ src/main/kotlin/day3/Day3.kt
@@ -3,15 +3,39 @@ package day3
 class Day3 {
     fun task1(input: String, rowStep: Int = 1, colStep: Int = 3): Long {
         val rows = input.lines()
-        val columns = rows.first().length
+        val columnsCount = rows.first().length
+        return solve(rows, rowStep, colStep, columnsCount)
+    }
+
+    private fun solve(
+        rows: List<String>,
+        rowStep: Int,
+        colStep: Int,
+        columnsCount: Int
+    ): Long {
         return rows.indices
             .filter { it % rowStep == 0 }
-            .map { it to (it * colStep) % columns }.sumOf { (row, col) ->
+            .mapIndexed { index, it -> it to (index * colStep) % columnsCount }.sumOf { (row, col) ->
                 if (rows[row][col] == '#')
                     1L
                 else
                     0L
             }
+    }
+
+    fun task2(input: String): Long {
+        val rows = input.lines()
+        val columnsCount = rows.first().length
+        val slopes = listOf(
+            1 to 1,
+            3 to 1,
+            5 to 1,
+            7 to 1,
+            1 to 2
+        )
+        return slopes.map { (colStep, rowStep) ->
+            solve(rows, rowStep, colStep, columnsCount)
+        }.fold(1L) { acc, count -> acc * count }
     }
 
 }
--- src/test/kotlin/day3/Day3Test.kt
+++ src/test/kotlin/day3/Day3Test.kt
@@ -18,6 +18,17 @@ class Day3Test {
     }
 
     @Test
+    fun task1ExampleDifferentStepTest() {
+        val input = File("src/test/resources/exampleDay3.txt")
+            .readText()
+
+        assertEquals(
+            2,
+            Day3().task1(input, 2, 1)
+        )
+    }
+
+    @Test
     fun task2SolutionTest() {
         val input = File("src/test/resources/task1Day3.txt")
             .readText()
@@ -25,6 +36,28 @@ class Day3Test {
         assertEquals(
             250,
             Day3().task1(input)
+        )
+    }
+
+    @Test
+    fun task2ExampleTest() {
+        val input = File("src/test/resources/exampleDay3.txt")
+            .readText()
+
+        assertEquals(
+            336,
+            Day3().task2(input)
+        )
+    }
+
+    @Test
+    fun task3SolutionTest() {
+        val input = File("src/test/resources/task1Day3.txt")
+            .readText()
+
+        assertEquals(
+            250,
+            Day3().task2(input)
         )
     }
 
