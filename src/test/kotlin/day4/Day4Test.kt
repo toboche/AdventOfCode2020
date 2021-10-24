ff5e87be8063ba0f446c6b386fbec5336cdf54cb
Tomasz Bochenek <tomasz.bochenek@dazn.com>
2021-10-24 20:51:05 +0200

    Day 4 task 1 solved

--- src/test/kotlin/day4/Day4Test.kt
+++ src/test/kotlin/day4/Day4Test.kt
@@ -15,4 +15,15 @@ class Day4Test {
             Day4().task1(input)
         )
     }
+
+    @Test
+    fun task1Test() {
+        val input = File("src/test/resources/task1Day4.txt")
+            .readText()
+
+        assertEquals(
+            2,
+            Day4().task1(input)
+        )
+    }
 }
