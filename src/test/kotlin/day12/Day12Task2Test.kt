fb2289349fde5df9c7cd68df24e6a23b2730dfa8
toboche <toboche@gmail.com>
2021-11-14 11:23:36 +0100

    Day 12 task 2 solved step 5 and example

--- src/test/kotlin/day12/Day12Task2Test.kt
+++ src/test/kotlin/day12/Day12Task2Test.kt
@@ -1,6 +1,7 @@
 package day12
 
 import org.junit.jupiter.api.Test
+import java.io.File
 import kotlin.test.assertEquals
 
 class Day12Task2Test {
@@ -39,6 +40,29 @@ class Day12Task2Test {
                 "N3",
                 "F7",
                 "R90"))
+        )
+    }
+
+    @Test
+    fun step5() {
+        assertEquals(
+            Day12Task2.Position(214, -72, 90, 4, -10),
+            Day12Task2().calculateFinalPosition(listOf("F10",
+                "N3",
+                "F7",
+                "R90",
+                "F11"))
+        )
+    }
+
+    @Test
+    fun example() {
+        val input = File("src/test/resources/exampleDay12.txt")
+            .readLines()
+
+        assertEquals(
+            286,
+            Day12Task2().task1(input)
         )
     }
 }
