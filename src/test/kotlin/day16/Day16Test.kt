539d250014c85eddcb2deced815360d0efc2cd57
toboche <toboche@gmail.com>
2021-11-20 17:51:37 +0100

    Day 16 task 1 solved

--- src/test/kotlin/day16/Day16Test.kt
+++ src/test/kotlin/day16/Day16Test.kt
@@ -6,11 +6,21 @@ import kotlin.test.assertEquals
 
 class Day16Test {
     @Test
-    fun task2Example() {
+    fun task1Example() {
         val input = File("src/test/resources/exampleDay16.txt")
 
         assertEquals(
             71,
+            Day16().task1(input)
+        )
+    }
+
+    @Test
+    fun task1() {
+        val input = File("src/test/resources/task1Day16.txt")
+
+        assertEquals(
+            29019,
             Day16().task1(input)
         )
     }
